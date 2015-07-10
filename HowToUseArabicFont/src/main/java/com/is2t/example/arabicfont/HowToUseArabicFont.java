ad29d4205b81083eb8028a7026432212dd1de2c2
skobak <skobak@is2t-intern.com>
2015-07-10 12:42:27 +0200

    change package name

--- HowToUseArabicFont/src/main/java/com/is2t/example/arabicfont/HowToUseArabicFont.java
+++ HowToUseArabicFont/src/main/java/com/is2t/example/arabicfont/HowToUseArabicFont.java
@@ -5,7 +5,7 @@
  * Use of this source code is governed by a BSD-style license that can be found at http://www.is2t.com/open-source-bsd-license/.
  */
 
-package com.is2t.example;
+package com.is2t.example.arabicfont;
 
 import ej.microui.io.Display;
 import ej.microui.io.Displayable;
